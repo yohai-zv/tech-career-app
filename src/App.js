d4ff7c209bd8baaa5f3f379054c40a56d99a6ab1
yohai.zvuloon@autodesk.com <yohai.zvuloon@autodesk.com>
2021-07-12 19:26:15 +0300

    Add index for login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import Login from './pages/Login/Login';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
 import Home from './HomePage/Home';
 import ActionMenu from './pages/ActionMenu';
 import Layout from './components/Layout';
@@ -16,6 +16,7 @@ export default function App() {
           <Route path='/login'>
             <Login />
           </Route>
+         
           <Route path='/'>
             <Home />
           </Route>
