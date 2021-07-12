591e8d80987a99ce4ea718708cad0347f14cacb0
yohai.zvuloon@autodesk.com <yohai.zvuloon@autodesk.com>
2021-07-12 19:11:58 +0300

    Update navigation bars

--- src/App.js
+++ src/App.js
@@ -9,35 +9,17 @@ export default function App() {
   return (
     <Router>
       <Layout>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/login">Login</Link>
-              </li>
-              <li>
-                <Link to="/actions">ActionMenu</Link>
-              </li>
-            </ul>
-          </nav>
-
-          {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-          <Switch>
-            <Route path="/actions">
-              <ActionMenu />
-            </Route>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </div>
+        <Switch>
+          <Route path='/actions'>
+            <ActionMenu />
+          </Route>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
       </Layout>
     </Router>
   );
